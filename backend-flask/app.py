be80c0eaf23722992c2a3485ee508a8b8b7fafe1
Stephanie Chester <schester81@gmail.com>
2023-03-09 19:55:48 +0000

    fixed error in app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -32,11 +32,20 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
-# X-RAY --------
+# X-RAY ----------
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+# Initialize tracing and an exporter that can send data to Honeycomb
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+
+# X-RAY ----------
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
-#Show this in the logs within the backend-flask app (STDOUT)
+# Show this in the logs within the backend-flask app (STDOUT)
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
 provider.add_span_processor(simple_processor)
 
@@ -45,7 +54,7 @@ tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
-#X-RAY --------------
+# X-RAY ----------
 XRayMiddleware(app, xray_recorder)
 
 # Initialize automatic instrumentation with Flask
